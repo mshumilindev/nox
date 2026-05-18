0e71f875fa0e9544c7a0c17d0c5d19b4cb57628b
Nikolay Lukyanov <Nikolay.Lukyanov@dataart.com>
2026-05-18 23:53:13 +0200

    Initial Commit

---
+++ Nox/NoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoxApp.swift
+//  Nox
+//
+//  Created by Mykola Lukianov on 18.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct NoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoxTests/NoxTests.swift
@@ -0,0 +1,18 @@
+//
+//  NoxTests.swift
+//  NoxTests
+//
+//  Created by Mykola Lukianov on 18.05.2026.
+//
+
+import Testing
+
+struct NoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
